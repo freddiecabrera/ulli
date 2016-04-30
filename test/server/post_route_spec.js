439113632ec121e49cf6d65d36f84c10d5616fb0
freddiecabrera <freddiecabrera18@yahoo.com>
2016-04-30 15:18:46 -0700

    finish test for PUT

--- test/server/post_route_spec.js
+++ test/server/post_route_spec.js
@@ -9,9 +9,11 @@ describe('POST server route', () => {
       .post('/footballers/footballer')
       .send({name: 'testplayer1', dataType: 'footballer'})
       .end((err, res) => {
+        if (err) res.status(400).send(err);
+        expect(res.status).to.equal(200);
         expect(res.body).to.be.an('object');
-        expect(res.body.name).to.be.an('string');
-        expect(res.body.dataType).to.be.an('string');
+        expect(res.body.name).to.be.a('string');
+        expect(res.body.dataType).to.be.a('string');
         Redis.DEL('testplayer1', (err, reply) => { if(err) return res.status(404).send(err); })
         done();
       })
@@ -22,18 +24,19 @@ describe('POST server route', () => {
     .post('/footballers/footballer')
     .send({name: 'testplayer2', dataType: 'footballer'})
     .end((err, res) => {
-      if (err) { return res.status(404).send(err).done(); }
+      if (err) return res.status(404).send(err).done();
       expect(res.text).to.equal('Data has already been submitted');
       expect(res.status).to.equal(202);
       done();
     })
   })
 
-  it('should return 404 if it does not contain both a name and a dataType', done => {
+  it('should return 401 if it does not contain both a name and a dataType', done => {
     request(app)
     .post('/footballers/footballer')
     .send({ notName: 'freddie', data: 'footballer'} )
     .end((err, res) => {
+      if (err) return res.status(400).send(err);
       expect(res.status).to.equal(401);
       expect(res.text).to.equal('A name and dataType is required');
       done();
