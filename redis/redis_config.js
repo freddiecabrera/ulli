72ac92614487b8681245565a4582e707ed35e18f
freddiecabrera <freddiecabrera18@yahoo.com>
2016-05-10 15:26:30 -0700

    testing

--- redis/redis_config.js
+++ redis/redis_config.js
@@ -1,19 +1,19 @@
-'use strict';
-
-const redis = require('redis'),
-url = require('url');
-
-exports.createClient = function (port_arg, host_arg, options) {
-    let client;
-    if (process.env.REDISTOGO_URL) {
-        const redisURL = url.parse(process.env.REDISTOGO_URL);
-        client = redis.createClient(redisURL.port, redisURL.hostname, options);
-        client.auth(redisURL.auth.split(":")[1]);
-    } else {
-        client = redis.createClient(port_arg, host_arg, options);
-    }
-
-    return client;
-};
-
-exports.redis = redis;
+// 'use strict';
+//
+// const redis = require('redis'),
+// url = require('url');
+//
+// exports.createClient = function (port_arg, host_arg, options) {
+//     let client;
+//     if (process.env.REDISTOGO_URL) {
+//         const redisURL = url.parse(process.env.REDISTOGO_URL);
+//         client = redis.createClient(redisURL.port, redisURL.hostname, options);
+//         client.auth(redisURL.auth.split(":")[1]);
+//     } else {
+//         client = redis.createClient(port_arg, host_arg, options);
+//     }
+//
+//     return client;
+// };
+//
+// exports.redis = redis;
