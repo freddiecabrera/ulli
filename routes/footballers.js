d7475094494a6cb60ec9179e7aae20b13aa58ebe
freddiecabrera <freddiecabrera18@yahoo.com>
2016-06-28 18:27:23 -0700

    switched all routes to mongodb

--- routes/footballers.js
+++ routes/footballers.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Player = require('../models/model')
+const Player = require('../models/model');
 
-// const keyGenerator = (name, dataType) => {
-//   if (dataType === 'footballer') return name.toLowerCase();
-//   else return `${name.toLowerCase()}'s ${dataType.toLowerCase()}`;
-// };
-
-router.get('/:footballer', (req, res) => {
-  const footballer = req.params.footballer.toLowerCase();
-  Redis.HGETALL(footballer, (err, data) => {
-    if (err) return res.status(400).send(err);
-    else data ? res.status(200).send(JSON.parse(data.footballerData)) : res.status(202).send('Data not in the DB');
+router.get('/:playerID', (req, res) => {
+  const playerID = req.params.playerID;
+  Player.findById(playerID, (err, player) => {
+    console.log(player);
+    err ? res.status(400).send(err) : res.send(player);
   });
 });
 
@@ -19,31 +14,34 @@ router.post('/footballer', (req, res) => {
   const newPlayer = req.body;
   Player.create(newPlayer, (err, player) => {
     err ? res.status(400).send(err) : res.send(player);
-  })
-})
-
-router.put('/footballer', (req, res) => {
-  if(req.body.name && req.body.dataType){
-    const keyName = keyGenerator(req.body.name, req.body.dataType);
-  } else return res.status(401).send('A name and dataType is required');
-
-  const footballerData = JSON.stringify(req.body);
-  Redis.HGETALL(keyName, (err, data) => {
-    if (err) { return res.status(400).send(err); }
-    else if (data) {
-      Redis.HMSET(keyName, { footballerData }, (err, reply) => {
-        if (err) return res.status(400).send(err);
-        else return res.status(200).json(req.body);
-      });
-    } else return res.status(202).send('Data is not in the DB');
   });
 });
 
-router.delete('/:footballer', (req, res) => {
-  const hash = req.params.footballer;
-  Redis.DEL(hash, (err, reply) => {
-    if (err) return res.status(400).send(err);
-    else reply === 0 ? res.status(202).send('Data not in the database') : res.status(200).send('Data deleted');
+router.put('/:playerID', (req, res) => {
+  const playerID = req.params.playerID;
+  const newPlayer = req.body;
+  const option = { new: true };
+
+  Player.findByIdAndUpdate(playerID, newPlayer, option, (err, player) => {
+    err ? res.status(400).send(err) : res.send(player);
+  });
+});
+
+router.delete('/beer/:id', function(req, res) {
+    BeerList.findByIdAndRemove(req.body._id, function(err, beer) {
+        if (err) {
+            return res.status(400).send(err);
+        } else {
+            return res.status(200).send("Successfully Removed!");
+        }
+    })
+});
+
+router.delete('/:playerID', (req, res) => {
+  const playerID = req.params.playerID;
+
+  Player.findByIdAndRemove(playerID, (err, player) => {
+    err ? res.status(400).send(err) : res.send('Player successfully removed from database');
   });
 });
 
