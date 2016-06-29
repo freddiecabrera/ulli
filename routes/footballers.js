4de7006d76b054974ad3158494f1e99a74563d5c
freddiecabrera <freddiecabrera18@yahoo.com>
2016-06-29 13:44:47 -0700

    No duplicate ballers :soccer:

--- routes/footballers.js
+++ routes/footballers.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Player = require('../models/model');
 
-router.get('/:playerID', (req, res) => {
-  const playerID = req.params.playerID;
-  Player.findById(playerID, (err, player) => {
+router.get('/:footballer', (req, res) => {
+  const footballer = req.params.footballer;
+  Player.findOne({ name: footballer }, (err, player) => {
     if (!player) return res.status(202).send('player not in the database');
     err ? res.status(400).send(err) : res.send(player);
   });
@@ -12,27 +12,45 @@ router.get('/:playerID', (req, res) => {
 
 router.post('/footballer', (req, res) => {
   const newPlayer = req.body;
-  Player.create(newPlayer, (err, player) => {
-    err ? res.status(400).send(err) : res.send(player);
+  Player.findOne({ name: newPlayer.name }, (err, player) => {
+    if (err) return res.status(400).send(err);
+    if (player === null) {
+      Player.create(newPlayer, (err, player) => {
+        err ? res.status(400).send(err) : res.send(player);
+      });
+    } else {
+      return res.status(202).send('Player already in the database');
+    }
   });
 });
 
-router.put('/:playerID', (req, res) => {
-  const playerID = req.params.playerID;
+router.put('/:footballer', (req, res) => {
+  const footballer = req.params.footballer;
   const newPlayer = req.body;
   const option = { new: true };
 
-  Player.findByIdAndUpdate(playerID, newPlayer, option, (err, player) => {
-    if (!player) return res.status(202).send('player not in the database');
-    err ? res.status(400).send(err) : res.send(player);
+  Player.findOne({ name: footballer }, (err, player) => {
+    if (player === null) {
+      return res.status(202).send('Player not in the database');
+    } else {
+      Player.findByIdAndUpdate(player._id, newPlayer, option, (err, player) => {
+        if (!player) return res.status(202).send('player not in the database');
+        err ? res.status(400).send(err) : res.send(player);
+      });
+    }
   });
 });
 
-router.delete('/:playerID', (req, res) => {
-  const playerID = req.params.playerID;
-
-  Player.findByIdAndRemove(playerID, (err, player) => {
-    err ? res.status(400).send(err) : res.send('Player successfully removed from database');
+router.delete('/:footballer', (req, res) => {
+  const footballer = req.params.footballer;
+  Player.findOne({ name: footballer }, (err, player) => {
+    if (player === null) {
+      return res.status(202).send('Player not in the database');
+    } else {
+      Player.findByIdAndRemove(player.id, (err, player) => {
+        err ? res.status(400).send(err) : res.send('Player successfully removed from database');
+      })
+    }
   });
 });
 
